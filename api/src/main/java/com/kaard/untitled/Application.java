2117aca2e57b51e5655aa7db70a2349fde773fd5
sass <sass@devtailor.com>
2019-02-14 10:13:40 +0200

    rename UntitledApplication

--- api/src/main/java/com/kaard/untitled/Application.java
+++ api/src/main/java/com/kaard/untitled/Application.java
@@ -3,11 +3,11 @@ package com.kaard.untitled;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class UntitledApplication {
+@SpringBootApplication(scanBasePackages = {"com.kaard.untitled.database"})
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UntitledApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
