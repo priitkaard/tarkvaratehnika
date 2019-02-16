1f29632b492a13bd690ce9901a3f42e562151751
sass <sass@devtailor.com>
2019-02-16 16:05:03 +0200

    add Swagger

--- api/src/main/java/com/qaengine/Application.java
+++ api/src/main/java/com/qaengine/Application.java
@@ -2,6 +2,13 @@ package com.qaengine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication()
 public class Application {
@@ -10,5 +17,17 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
+    @Configuration
+    @EnableSwagger2
+    public class SpringFoxConfig {
+        @Bean
+        public Docket apiDocket() {
+            return new Docket(DocumentationType.SWAGGER_2)
+                    .select()
+                    .apis(RequestHandlerSelectors.any())
+                    .paths(PathSelectors.any())
+                    .build();
+        }
+    }
 }
 
