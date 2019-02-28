f504f9d5354144f4db093880da50284b48ebf405
Priit Kaard <priit.kaard@coolbet.com>
2019-02-28 08:50:25 +0200

    Category fix

--- api/src/main/java/com/qaengine/models/Question.java
+++ api/src/main/java/com/qaengine/models/Question.java
@@ -29,7 +29,7 @@ public class Question {
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "category_id", nullable = false)
+    @JoinColumn(name = "category_id") //  nullable = false
     private Category category;
 
     @Column(nullable = false)
