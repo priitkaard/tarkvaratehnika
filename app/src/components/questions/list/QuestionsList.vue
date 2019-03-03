7b6f9bc1c4cad7e6fab9400becae9919676a66b2
Priit Käärd <prkaar@ttu.ee>
2019-03-03 20:53:16 +0200

    Travolta was missing when no results

--- app/src/components/questions/list/QuestionsList.vue
+++ app/src/components/questions/list/QuestionsList.vue
@@ -4,6 +4,15 @@
                 v-for="question in questions"
                 v-bind:key="question.id"
                 v-bind:question="question" />
+        <div v-if="questions.length === 0"
+            class="QuestionsList__no_results">
+            <div>
+                <img src="@/assets/img/travolta.gif" height="100" />
+            </div>
+            <div class="QuestionsList__no_results_text">
+                No questions found for your search...
+            </div>
+        </div>
     </div>
 </template>
 
@@ -22,8 +31,23 @@
     }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+    @import '../../../assets/styles/_colors';
+    @import '../../../assets/styles/_mixins';
+
     .QuestionsList {
         margin-top: -10px;
+
+        &__no_results {
+            padding: 20px;
+            text-align: center;
+            background-color: white;
+            color: black;
+            margin-top: 10px;
+            @include shadow-box;
+            &_text {
+                margin-top: 10px;
+            }
+        }
     }
 </style>
