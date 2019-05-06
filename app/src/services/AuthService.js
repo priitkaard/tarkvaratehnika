9c1cd62ef2239293030955b1858a308d8c26da06
jajann <jarekx.j@gmail.com>
2019-05-06 14:43:06 +0300

    points showing next to username

--- app/src/services/AuthService.js
+++ app/src/services/AuthService.js
@@ -3,15 +3,13 @@ import store from '../store';
 import apiService, { handleErrors } from './ApiService';
 
 export default {
-    doLogin(token, username) {
+    doLogin(token, username, points) {
         localStorage.setItem('authToken', token);
-        localStorage.setItem('username', username);
-        store.dispatch('auth/logIn', { username });
+        store.dispatch('auth/logIn', { username, points });
     },
 
     logOut() {
         localStorage.removeItem('authToken');
-        localStorage.removeItem('username');
         store.dispatch('auth/logOut');
     },
 
@@ -23,8 +21,10 @@ export default {
             handleErrors(err);
         }
 
+        let points = await apiService.get(`/user/points/${username}`);
+
         const token = response.headers.authorization;
-        this.doLogin(token, username);
+        this.doLogin(token, username, points);
 
         return token;
     },
@@ -38,5 +38,6 @@ export default {
         } catch (err) {
             handleErrors(err);
         }
-    }
+    },
+
 }
--- app/src/store/modules/auth.js
+++ app/src/store/modules/auth.js
@@ -2,16 +2,19 @@ export default {
     namespaced: true,
     state: {
         isLoggedIn: Boolean(localStorage.getItem('authToken')),
-        username: localStorage.getItem('username'),
+        username: null,
+        points: null,
     },
     actions: {
         logIn(context, payload) {
             context.commit('setLoggedIn', true);
             context.commit('setUsername', payload.username);
+            context.commit('setPoints', payload.points);
         },
         logOut(context) {
             context.commit('setLoggedIn', false);
             context.commit('setUsername', null);
+            context.commit('setPoints', null);
         }
     },
     mutations: {
@@ -21,5 +24,8 @@ export default {
         setUsername(state, username) {
             state.username = username;
         },
+        setPoints(state, points){
+            state.points = points;
+        }
     }
 }
